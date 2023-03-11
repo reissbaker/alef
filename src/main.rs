510a17318719bd4c9a5d9d38d64f89663d2e2c7a
Matt Baker <matthew.reiss.baker@gmail.com>
2023-03-11 00:00:08 -0800

    First pass at type values (i.e. non-generics/conditionals/hkts)

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,7 @@ use ast::parser::parse;
 use ast::errors::format_error;
 use symbols::SymbolTable;
 use ir::{to_ir_vec, DisplayFromSymbol};
+use types::{Signature, Nominal, TypeComparison, TypeVal, Primitive, Algebraic, Static};
 use thiserror::Error;
 
 #[derive(Error, Debug, Diagnostic)]
@@ -60,6 +61,46 @@ fn main() -> miette::Result<()> {
             for (pos, expr) in ir.iter().enumerate() {
                 println!("\n{}: {:?}", pos + 1, expr.to_pretty_string(&table));
             }
+
+            let i = TypeVal::Primitive(Primitive::I64);
+            let i_9 = TypeVal::Static(Static::I64(9));
+            let i9_or_i = TypeVal::Algebraic(Algebraic::Sum(
+                    vec![i.clone(), i_9.clone()]
+            ));
+            let i9_and_i = TypeVal::Algebraic(Algebraic::Intersection(
+                    vec![i.clone(), i_9.clone()]
+            ));
+            println!("\n\n9 is subtype of i64: {}", i_9.is_subtype(&i));
+            println!("i64 is subtype of i9: {}", i.is_subtype(&i_9));
+
+            println!("\n\n9 is subtype of sum(i64, 9): {}", i_9.is_subtype(&i9_or_i));
+            println!("i64 is subtype of sum(i64, 9): {}", i.is_subtype(&i9_or_i));
+            println!("sum(i64, 9) is subtype of i64: {}", i9_or_i.is_subtype(&i));
+            println!("sum(i64, 9) is subtype of 9: {}", i9_or_i.is_subtype(&i_9));
+
+            println!("\n\n9 is subtype of intersect(i64, 9): {}", i_9.is_subtype(&i9_and_i));
+            println!("i64 is subtype of intersect(i64, 9): {}", i.is_subtype(&i9_and_i));
+            println!("intersect(i64, 9) is subtype of i64: {}", i9_and_i.is_subtype(&i));
+            println!("intersect(i64, 9) is subtype of 9: {}", i9_and_i.is_subtype(&i_9));
+
+            let sig_i = TypeVal::Signature(Signature::new(
+                vec![ i.clone() ],
+                i.clone(),
+            ));
+            let sig_9 = TypeVal::Signature(Signature::new(
+                vec![ i_9.clone() ],
+                i_9.clone(),
+            ));
+            println!("\n\n(i64) -> i64 is subtype of (9) -> 9: {}", sig_i.is_subtype(&sig_9));
+            println!("(9) -> 9 is subtype of (i64) -> i64: {}", sig_9.is_subtype(&sig_i));
+
+            let bitmap_sym = table.symbol("Bitmap".into());
+            let tex_sym = table.symbol("Texture".into());
+            let Bitmap = TypeVal::Nominal(Nominal::new(bitmap_sym));
+            let Texture = TypeVal::Nominal(Nominal::new(tex_sym));
+            println!("\n\nBitmap nominal is subtype of Texture nominal: {}", Bitmap.is_subtype(&Texture));
+            println!("Texture nominal is subtype of Bitmap nominal: {}", Texture.is_subtype(&Bitmap));
+            println!("Texture nominal is subtype of Texture nominal: {}", Texture.is_subtype(&Texture));
 
             Ok(())
         }
--- src/types.rs
+++ src/types.rs
@@ -1,50 +1,224 @@
 use std::collections::HashMap;
 use crate::ir::Ir;
-use crate::symbols::Symbol;
+use crate::symbols::{Symbol, SymbolTable};
 
+#[derive(Clone, Debug)]
 pub struct TypeCondArm {
     condition: Type,
     body: Vec<Type>,
 }
 
+#[derive(Clone, Debug)]
 pub struct TypeElseArm {
     body: Vec<Type>,
 }
 
+#[derive(Clone, Debug)]
 pub struct WhereType {
     args: Vec<TypeArg>,
     body: Vec<Type>,
 }
 
+#[derive(Clone, Debug)]
 pub enum TypeArg {
     Unbound,
     Bound(Type),
 }
 
-pub enum Type {
+pub trait TypeComparison {
+    fn is_subtype(&self, other: &TypeVal) -> bool;
+
+    fn algebraic_subtype(&self, other: &Algebraic) -> bool {
+        match other {
+            Algebraic::Sum(types) => {
+                types.iter().any(|t| self.is_subtype(t))
+            }
+            Algebraic::Intersection(types) => {
+                types.iter().all(|t| self.is_subtype(t))
+            }
+        }
+    }
+}
+
+#[derive(Clone, Copy, PartialEq, Eq, Debug)]
+pub enum Primitive {
     I64,
-    IStatic(i64),
     U64,
-    UStatic(u64),
     F64,
-    FStatic(f64),
     Char,
-    CharStatic(char),
     Str,
-    StrStatic(String),
-    True,
-    False,
     Bool,
     Nil,
-    Never,
-    Sum(Vec<Type>),
-    Intersection(Vec<Type>),
-    Fn(Vec<Type>, Box<Type>),
-    Structural(Vec<(Symbol, Type)>),
+    Never
+}
+
+impl TypeComparison for Primitive {
+    fn is_subtype(&self, other: &TypeVal) -> bool {
+        // If you're a primitive, you are a subtype of yourself
+        // TODO: Conds + Type Aliases
+        match other {
+            TypeVal::Primitive(other_prim) => {
+                other_prim == self
+            }
+            TypeVal::Static(_) => false,
+            TypeVal::Algebraic(algebraic_type) => self.algebraic_subtype(algebraic_type),
+            _ => false,
+        }
+    }
+}
+
+#[derive(Clone, PartialEq, Debug)]
+pub enum Static {
+    I64(i64),
+    U64(u64),
+    F64(f64),
+    Char(char),
+    Str(String),
+    True,
+    False,
+}
+
+impl Static {
+    fn primitive_supertype(&self) -> Primitive {
+        match self {
+            Static::I64(_) => Primitive::I64,
+            Static::U64(_) => Primitive::U64,
+            Static::F64(_) => Primitive::F64,
+            Static::Char(_) => Primitive::Char,
+            Static::Str(_) => Primitive::Str,
+            Static::True => Primitive::Bool,
+            Static::False => Primitive::Bool,
+        }
+    }
+}
+
+impl TypeComparison for Static {
+    fn is_subtype(&self, other: &TypeVal) -> bool {
+        match other {
+            TypeVal::Primitive(primitive) => {
+                *primitive == self.primitive_supertype()
+            },
+            TypeVal::Static(other_static) => {
+                self == other_static
+            }
+            TypeVal::Algebraic(algebraic_type) => self.algebraic_subtype(algebraic_type),
+            _ => false,
+        }
+    }
+}
+
+#[derive(Clone, Debug)]
+pub enum Algebraic {
+    Sum(Vec<TypeVal>),
+    Intersection(Vec<TypeVal>),
+}
+
+impl TypeComparison for Algebraic {
+    fn is_subtype(&self, other: &TypeVal) -> bool {
+        match self {
+            Algebraic::Sum(types) => {
+                types.iter().all(|t| t.is_subtype(other))
+            }
+            Algebraic::Intersection(types) => {
+                types.iter().any(|t| t.is_subtype(other))
+            }
+        }
+    }
+}
+
+#[derive(Clone, Debug)]
+pub enum TypeFn {
     Cond(Vec<TypeCondArm>, Option<TypeElseArm>),
     Where(WhereType),
+}
+
+impl TypeFn {
+    fn compute(&self, args: Vec<&Type>) -> &Type {
+        // TODO: actually eval these
+        match self {
+            TypeFn::Cond(cond_arms, maybe_else) => {
+                &cond_arms.iter().nth(0).unwrap().condition
+            }
+            TypeFn::Where(where_type) => {
+                where_type.body.iter().nth(0).unwrap()
+            }
+        }
+    }
+}
+
+#[derive(Clone, Debug)]
+pub struct Signature {
+    args: Vec<TypeVal>,
+    result: Box<TypeVal>,
+}
+
+impl Signature {
+    pub fn new(args: Vec<TypeVal>, ret: TypeVal) -> Signature {
+        Signature {
+            args,
+            result: Box::new(ret),
+        }
+    }
+}
+
+impl TypeComparison for Signature {
+    fn is_subtype(&self, other: &TypeVal) -> bool {
+        match other {
+            TypeVal::Algebraic(alg_type) => self.algebraic_subtype(alg_type),
+            TypeVal::Signature(other_sig) => {
+                if self.args.len() != other_sig.args.len() {
+                    return false;
+                }
+                for (i, arg) in self.args.iter().enumerate() {
+                    if !arg.is_subtype(&other_sig.args[i]) {
+                        return false;
+                    }
+                }
+                self.result.is_subtype(&other_sig.result)
+            }
+            // All other types fail
+            _ => false,
+        }
+    }
+}
+
+#[derive(Clone, Debug)]
+pub struct Nominal {
+    name: Symbol,
+}
+
+impl Nominal {
+    pub fn new(name: Symbol) -> Nominal {
+        Nominal { name }
+    }
+}
+
+impl TypeComparison for Nominal {
+    fn is_subtype(&self, other: &TypeVal) -> bool {
+        match other {
+            TypeVal::Algebraic(alg_type) => self.algebraic_subtype(alg_type),
+            TypeVal::Nominal(nominal) => self.name == nominal.name,
+            // All other types are not a match
+            _ => false,
+        }
+    }
+}
+
+#[derive(Clone, Debug)]
+pub enum TypeVal {
+    Primitive(Primitive),
+    Static(Static),
+    Algebraic(Algebraic),
+    Signature(Signature),
+    Nominal(Nominal),
+}
+
+#[derive(Clone, Debug)]
+pub enum Type {
+    Val(TypeVal),
+    Fn(TypeFn),
     Kind(Vec<TypeArg>),
-    Type(Box<Type>),
+    TypeAlias(Box<TypeVal>),
 }
 
 pub struct Scope {
@@ -62,8 +236,46 @@ pub struct TypeError {
     msg: String,
 }
 
+impl TypeComparison for TypeVal {
+    fn is_subtype(&self, other: &TypeVal) -> bool {
+        match self {
+            TypeVal::Primitive(primitive) => primitive.is_subtype(other),
+            TypeVal::Static(static_type) => static_type.is_subtype(other),
+            TypeVal::Algebraic(alg_type) => alg_type.is_subtype(other),
+            TypeVal::Signature(sig_type) => sig_type.is_subtype(other),
+            TypeVal::Nominal(nominal) => nominal.is_subtype(other),
+        }
+    }
+}
+
 impl Type {
-    fn check<'a>(&self, scope: &Scope, ir: Ir<'a>) -> Result<(), Vec<TypeError>> {
-        Err(vec![])
+    fn check<'a>(&self, table: &SymbolTable, scope: &Scope, ir: Ir<'a>) -> Result<(), Vec<TypeError>> {
+        match self {
+            Type::Val(TypeVal::Primitive(Primitive::I64)) => {
+                match ir {
+                    Ir::Int(_, _) => {
+                        Ok(())
+                    }
+                    Ir::Id(id) => {
+                        if let Some(id_type) = scope.get_type(id.1) {
+                            /*
+                            if id_type.is_subtype(self) {
+                                return Ok(())
+                            }
+                            */
+                            return Err(vec![])
+                        }
+                        Err(vec![])
+                    }
+                    _ => {
+                        Err(vec![])
+                    }
+                }
+            }
+
+            _ => {
+                Err(vec![])
+            }
+        }
     }
 }
